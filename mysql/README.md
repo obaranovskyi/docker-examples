7796402a62bc0efd89b125ad3072c873f6708177
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2023-12-25 15:14:58 +0200

    Auto git push ...

--- mysql/README.md
+++ mysql/README.md
@@ -1,3 +1,10 @@
+## Configure `mycli`
+
+_~/.myclirc_:
+```
+[alias_dsn]
+local = mysql://myuser:mypassword@127.0.0.1:3307/bookstore
+```
 
 ## `mycli`
 
--- mysql/connect_mycli.sh
+++ mysql/connect_mycli.sh
@@ -1,8 +1,3 @@
-# before add to ~/.myclirc
-# [alias_dsn]
-# local = mysql://myuser:mypassword@127.0.0.1:3307/bookstore
-
-
 # It's possible that you might try 2+ times to connect
 
 mycli local
