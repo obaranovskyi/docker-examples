fe196fce5bc469583d767151e5285abf1868b5fe
olegbaranovskyi <oleh.baranovskyi.dev.acc@gmail.com>
2025-11-17 16:38:31 +0200

    Auto git push ...

---
+++ mssql/run.sh
@@ -0,0 +1,7 @@
+docker run \
+  -e "ACCEPT_EULA=Y" \
+  -e "SA_PASSWORD=YourStrong!Passw0rd" \
+  -p 1433:1433 \
+  --name my-mssql \
+  -v "$(pwd)/mssql-data":/var/opt/mssql \
+  -d mcr.microsoft.com/mssql/server:2022-latest
